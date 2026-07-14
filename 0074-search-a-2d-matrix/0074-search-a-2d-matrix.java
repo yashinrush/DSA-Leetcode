a5407dfb3baab7e1d8e4d3e2d1b9fe85a4e8543d
Yash Sonawane <157393874+yashinrush@users.noreply.github.com>
2026-07-15 01:37:24 +0530

    Time: 0 ms (100%), Space: 43.9 MB (43.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,11 +3,30 @@ class Solution {
         //bruteforce
         int row = matrix.length;
         int col = matrix[0].length;
-        for(int i=0; i<row; i++){//this loop help to find out the number of rows in the matrix
-            for(int j=0; j<col; j++){// this loop help to find out the number of colums in the matrix
-                if(matrix[i][j]==target){// this loop help to compare the target with the elements of the matrix
-                    return true;
-                }
+        // for(int i=0; i<row; i++){//this loop help to find out the number of rows in the matrix
+        //     for(int j=0; j<col; j++){// this loop help to find out the number of colums in the matrix
+        //         if(matrix[i][j]==target){// this loop help to compare the target with the elements of the matrix
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        //binary search approch
+        int start = 0;
+        int end = row*col-1;
+
+        while(start<= end){
+            int mid = start + (end - start) / 2;
+
+            int midElement = matrix[mid / col][mid % col];
+
+            if (midElement == target) {
+                return true;
+            } else if (midElement < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return false;
