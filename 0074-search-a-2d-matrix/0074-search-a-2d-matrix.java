a42cdda409ff0f774063e4fad80b513b8d43694f
Yash Sonawane <157393874+yashinrush@users.noreply.github.com>
2026-07-15 01:29:35 +0530

    Time: 0 ms (100%), Space: 43.9 MB (62.46%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,9 +3,9 @@ class Solution {
         //bruteforce
         int row = matrix.length;
         int col = matrix[0].length;
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(matrix[i][j]==target){
+        for(int i=0; i<row; i++){//this loop help to find out the number of rows in the matrix
+            for(int j=0; j<col; j++){// this loop help to find out the number of colums in the matrix
+                if(matrix[i][j]==target){// this loop help to compare the target with the elements of the matrix
                     return true;
                 }
             }
