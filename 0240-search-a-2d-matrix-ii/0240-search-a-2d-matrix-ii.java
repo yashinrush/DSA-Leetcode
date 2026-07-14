d71fbb82e4475cd530b46bfc35a37902b7e28a1b
Yash Sonawane <157393874+yashinrush@users.noreply.github.com>
2026-07-15 02:05:25 +0530

    Time: 3 ms (89.75%), Space: 48.1 MB (64.61%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,45 +1,32 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        // // Start from the top-right corner of the matrix
-        // int row = 0;
-        // int col = matrix[0].length - 1;
+        // Start from the top-right corner
+        int row = 0;
+        int col = matrix[0].length - 1;
 
-        // // Continue until we go outside the matrix
-        // while (row < matrix.length && col >= 0) {
+        // Continue until we go out of the matrix
+        while (row < matrix.length && col >= 0) {
 
-        //     // If the current element is the target, return true
-        //     if (matrix[row][col] == target) {
-        //         return true;
-
-        //     // If the target is smaller than the current element,
-        //     // move LEFT because all elements below are even larger.
-        //     } else if (target < matrix[row][col]) {
-        //         col--;
-
-        //     // If the target is greater than the current element,
-        //     // move DOWN because all elements to the left are smaller.
-        //     } else {
-        //         row++;
-        //     }
-        // }
-
-        // // Target was not found in the matrix
-        // return false;
-
-        //binary search.....................................
-        int row =0 ;
-        int col = matrix[0].length-1;
-         while(row<matrix.length && col>=0){
-            if(matrix[row][col]==target){
+            // If the current element is the target, return true
+            if (matrix[row][col] == target) {
                 return true;
             }
-            else if(matrix[row][col]<target){
+
+            // If the current element is smaller than the target,
+            // move DOWN because all elements to the left are even smaller.
+            else if (matrix[row][col] < target) {
                 row++;
             }
-            else{
+
+            // If the current element is larger than the target,
+            // move LEFT because all elements below are even larger.
+            else {
                 col--;
             }
-        }return false;
+        }
+
+        // Target is not present in the matrix
+        return false;
     }
 }
