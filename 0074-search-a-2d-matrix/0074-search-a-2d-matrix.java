7084077ff2d76d1eb5ffaba8aecb4838c1b55144
Yash Sonawane <157393874+yashinrush@users.noreply.github.com>
2026-07-15 01:28:12 +0530

    Time: 0 ms (100%), Space: 44.1 MB (24.52%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //bruteforce
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
